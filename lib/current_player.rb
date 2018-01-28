4512c9b0b6b9dd79ee901beb45ac91b1cf7f9238
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-01-28 17:31:32 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,6 @@
 def turn_count(board)
-  turn_count = 0
+  count = 0
   board.each do |board_index|
-    while board_index == true || turn_count < board.length
-      turn_count += 1
-      return turn_count
-    end
+
   end
 end
