faf7995c0dac6f13f81192a9faabb3d021021f88
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-01-28 17:39:47 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,10 @@ def turn_count(board)
   count = 0
   board.each do |board_space|
     if board_space != " "
-    count += 1
-  else
-    count += 0
+      count += 1
+    else
+      count += 0
+    end
   end
+  return count
 end
