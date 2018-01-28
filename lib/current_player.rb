fd42ffabbaf855f00b9ed57403a2452304496e75
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-01-28 17:28:19 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   turn_count = 0
   board.each do |board_index|
-    if board_index == "X" || board_index == "O"
+    if board_index == true || turn_count < board.length
       turn_count += 1
     else
       break
