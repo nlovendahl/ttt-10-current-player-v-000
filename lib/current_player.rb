892f59d758179c09f711a135a277a105e4d14b7d
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-01-28 17:44:48 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+def turn_count(board)
+  count = 0
+  board.each do |board_space|
+    if board_space != " "
+      count += 1
+    else
+      count += 0
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  (turn_count(board) % 2 == 0) ? "X" : "O"
+end
