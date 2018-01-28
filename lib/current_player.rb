40308d3b79239a52f39f156842d54f7088879d4d
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-01-28 17:33:23 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,11 @@
 def turn_count(board)
   count = 0
   board.each do |space|
-    
+    if board[space] == "O" || board[space] == "X"
+      count += 1
+      return count
+    else
+      turn_count(board)
+    end
   end
 end
